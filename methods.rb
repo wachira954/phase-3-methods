c1a57f63049c1ad3581374a30cb817a0e7088759
wachira <wachiraa954@gmail.com>
2022-08-25 13:59:18 +0300

    assgn done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer ()
+    puts "Hello, programmer!"
+ end
+
+ def greet(name) 
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    p num1+num2
+end 
+
+def halve(number)
+    if number.class == Integer
+      return number/2
+    end
+  
+    return nil
+  end
